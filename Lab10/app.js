352162b14171b7a40471510c19f80de3cc4f33df
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2025-04-17 22:19:16 -0600

    Add files via upload

---
+++ Lab10/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware para leer datos del formulario
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('views'));
+
+// Rutas
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/formulario', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'formulario.html'));
+});
+
+app.get('/info', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'info.html'));
+});
+
+app.post('/formulario', (req, res) => {
+  const datos = `Nombre: ${req.body.nombre}, Mensaje: ${req.body.mensaje}\n`;
+  fs.appendFile('datos.txt', datos, (err) => {
+    if (err) throw err;
+    res.send('<h2>Datos recibidos y guardados en datos.txt</h2><a href="/">volver</a>');
+  });
+});
+
+
+app.use((req, res) => {
+    res.status(404).send("Error: 404 - Pagina no encontrada");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
